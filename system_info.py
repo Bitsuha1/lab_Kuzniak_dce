662c1832ce94aaeaf66dd90b019b70c27e5eb7e2
Bitsuha1 <mykhailo.kuzniak.ki.2022@lpnu.ua>
2025-05-09 00:34:51 +0300

    Lab1: Add average cpu load stat

--- system_info.py
+++ system_info.py
@@ -4,18 +4,22 @@ import platform
 
 import psutil
 
+def append_with_limit(buff: list[int], item: int, n: int):
+    buff.append(item)
+    while len(buff) > n:
+        buff.pop(0)
+
 class SystemInfo:
     def __init__(self):
+        self.update()
+    
+    def update(self):
         self.os: str = f"{platform.system()} {platform.release()}"
         self.processor: str = platform.processor()
         self.p_cpu_count: int = psutil.cpu_count(logical=False)
         self.l_cpu_count: int = psutil.cpu_count(logical=True)
         self.ram: int = psutil.virtual_memory().total
         self.partitions: list[str] = [partition.device for partition in psutil.disk_partitions()]
-        self.cpu_load: int = psutil.cpu_percent()
-        self.ram_load: int = psutil.virtual_memory().percent
-    
-    def update(self):
         self.cpu_load: int = psutil.cpu_percent(interval=1)
         self.ram_load: int = psutil.virtual_memory().percent
 
@@ -34,8 +38,12 @@ def display_system_info(info: SystemInfo):
 
 if __name__ == "__main__":
     system_info = SystemInfo()
+    cpu_last_20: list[int] = []
 
     while True:
         clear_screen()
         display_system_info(system_info)
+        append_with_limit(cpu_last_20, system_info.cpu_load, 20)
+        if len(cpu_last_20):
+            print(f"\nAverage cpu load of last 20 updates: {(sum(cpu_last_20) / len(cpu_last_20)):.2f} %")
         system_info.update()
